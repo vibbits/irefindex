115b7cf535f459aa3ec5eeb0b9b3d9b86293d93d
Paul Boddie <paul.boddie@biotek.uio.no>
2012-01-10 19:28:03 +0100

    Fixed the interactors report by handling interactors having different identifiers that simultaneously provide and fail to provide sequence information. Improved the assignment report to show information similar to Table 3 in the iRefIndex paper.

--- reports/assignments_by_source.sql
+++ reports/assignments_by_source.sql
@@ -44,4 +44,48 @@ create temporary table tmp_unassigned_by_sequences as
 
 \copy tmp_unassigned_by_sequences to '<directory>/unassigned_by_sequences'
 
+-- Show the coverage of each source (like Table 3 from the iRefIndex paper).
+
+create temporary table tmp_assignment_coverage as
+    select coalesce(assigned.source, assignable.source, unassignable.source) as source,
+
+        -- Total interactors.
+        coalesce(assigned.total, 0) + coalesce(assignable.total, 0) + coalesce(unassignable.total, 0) as total,
+        -- Total assignments.
+        coalesce(assigned.total, 0) as assigned_total,
+        -- Total unassigned.
+        coalesce(assignable.total, 0) + coalesce(unassignable.total, 0) as unassigned_total,
+        -- Percent coverage.
+        round(
+            cast(
+                cast(coalesce(assigned.total, 0) as real) / (coalesce(assignable.total, 0) + coalesce(unassignable.total, 0) + coalesce(assigned.total, 0)) * 100
+                as numeric
+                ), 2
+            ) as coverage,
+        -- Assignable.
+        coalesce(assignable.total, 0) as assignable_total,
+        -- Unassignable.
+        coalesce(unassignable.total, 0) as unassignable_total,
+        -- Unique proteins.
+        coalesce(sequences.total) as sequences_total
+
+    from tmp_assignments_by_source as assigned
+    full outer join (
+        select source, total
+        from tmp_unassigned_by_source
+        where havesequences
+        ) as assignable
+        on assigned.source = assignable.source
+    full outer join (
+        select source, total
+        from tmp_unassigned_by_source
+        where not havesequences
+        ) as unassignable
+        on assigned.source = unassignable.source
+    full outer join tmp_sequences_by_source as sequences
+        on assigned.source = sequences.source
+    order by coalesce(assigned.source, assignable.source, unassignable.source);
+
+\copy tmp_assignment_coverage to '<directory>/assignment_coverage_by_source'
+
 rollback;
--- reports/interactors_by_source.sql
+++ reports/interactors_by_source.sql
@@ -2,16 +2,41 @@ begin;
 
 -- Show the number of distinct interactors by data source.
 
-create temporary table tmp_interactors_by_source as
+create temporary table tmp_interactors_by_source_and_sequence as
     select source, count(distinct array[filename, cast(entry as varchar), interactorid]) as total, havesequence
     from (
-        select source, filename, entry, interactorid, case
-            when refsequence is null then false
-            else true end as havesequence
-        from xml_xref_interactor_sequences
-        ) as X
+        select source, filename, entry, interactorid,
+            array_length(sequences, 1) = 1 and sequences[1] is null as havesequence
+        from (
+            select source, filename, entry, interactorid, array_accum(refsequence) as sequences
+            from xml_xref_interactor_sequences
+            group by source, filename, entry, interactorid
+            ) as X
+        ) as Y
     group by source, havesequence
     order by source, havesequence;
+
+-- The output table has the following form:
+--
+-- <source> <total interactors> <total with sequence> <total without sequence>
+
+create temporary table tmp_interactors_by_source as
+    select coalesce(with_sequence.source, without_sequence.source) as source,
+        coalesce(with_sequence.total, 0) + coalesce(without_sequence.total, 0) as total,
+        coalesce(with_sequence.total, 0) as total_with_sequence,
+        coalesce(without_sequence.total, 0) as total_without_sequence
+    from (
+        select source, total
+        from tmp_interactors_by_source_and_sequence
+        where havesequence
+        ) as with_sequence
+    full outer join (
+        select source, total
+        from tmp_interactors_by_source_and_sequence
+        where not havesequence
+        ) as without_sequence
+        on with_sequence.source = without_sequence.source
+    order by source;
 
 \copy tmp_interactors_by_source to '<directory>/interactors_by_source'
 
