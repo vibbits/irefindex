770bd883f0ca7d8a54884c9c13d48216d25b5ec0
Paul Boddie <paul.boddie@biotek.uio.no>
2012-02-17 13:26:09 +0100

    Added grid-based comparisons of sources for ROG and RIG identifiers.

--- reports/rigids_by_source.sql
+++ reports/rigids_by_source.sql
@@ -25,4 +25,36 @@ create temporary table tmp_rigids_shared_by_sources as
 
 \copy tmp_rigids_shared_by_sources to '<directory>/rigids_shared_by_sources'
 
+-- Make a grid that can be displayed using...
+-- column -s ',' -t rigids_shared_as_grid
+
+create temporary table tmp_rigids_shared_as_grid as
+
+    -- Make a header.
+
+    select array_to_string(array_cat(array[cast('-' as varchar)], array_accum(source)), ',')
+    from tmp_rigids_by_source
+    union all (
+
+        -- Make each row with the source in the first column.
+
+        select array_to_string(array_cat(array[source1], array_accum(coalesce(cast(total as varchar), '-'))), ',')
+        from (
+            select S.source1, S.source2, R.total
+            from tmp_rigids_shared_by_sources as R
+            right outer join (
+                select S1.source as source1, S2.source as source2
+                from tmp_rigids_by_source as S1
+                cross join tmp_rigids_by_source as S2
+                ) as S
+                on R.source1 = S.source1
+                and R.source2 = S.source2
+            order by S.source1, S.source2
+            ) as X
+        group by source1
+        order by source1
+    );
+
+\copy tmp_rigids_shared_as_grid to '<directory>/rigids_shared_as_grid'
+
 rollback;
