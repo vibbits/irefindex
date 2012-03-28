db22b37dbacf8d8821fd863bcc2847cb67ea8355
Paul Boddie <paul.boddie@biotek.uio.no>
2012-03-28 17:41:41 +0200

    Fixed the export to produce canonical ROG identifiers instead of specific ROG identifiers.

--- sql/export_irefindex_rogids_canonical.sql
+++ sql/export_irefindex_rogids_canonical.sql
@@ -3,18 +3,20 @@ begin;
 -- Note that the ordering requires an appropriate locale.
 
 create temporary table tmp_interaction_rogids_canonical as
-    select distinct rigid, rogids
+    select distinct rigid, crogids
     from (
-        select rigid, array_to_string(array_accum(rogid), '') as rogids
+        select rigid, array_to_string(array_accum(crogid), '') as crogids
         from (
 
             -- Select all ROG identifiers, which can include the same ROG identifier
             -- for multiple participants.
 
-            select source, filename, entry, interactionid, rigid, rogid
-            from irefindex_interactions
+            select source, filename, entry, interactionid, rigid, crogid
+            from irefindex_interactions as I
+            inner join irefindex_rogids_canonical as C
+                on I.rogid = C.rogid
 
-            order by source, filename, entry, interactionid, rigid, rogid -- collate "C" for PostgreSQL 9.1
+            order by source, filename, entry, interactionid, rigid, crogid -- collate "C" for PostgreSQL 9.1
             ) as X
 
         group by source, filename, entry, interactionid, rigid
