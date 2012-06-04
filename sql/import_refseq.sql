75712f90fbbefec43f0ea23e0fa577ad2b30675a
Paul Boddie <paul.boddie@biotek.uio.no>
2012-06-04 12:45:36 +0200

    Fixed the RefSeq import to work with the additional "missing" columns.

--- sql/import_refseq.sql
+++ sql/import_refseq.sql
@@ -11,9 +11,21 @@ create temporary table tmp_refseq_proteins (
     length integer not null
 );
 
+create temporary table tmp_refseq_identifiers (
+    accession varchar not null,
+    dblabel varchar not null,
+    refvalue varchar not null,
+    position integer not null
+);
+
+create temporary table tmp_refseq_nucleotides (
+    nucleotide varchar not null,
+    protein varchar not null
+);
+
 \copy tmp_refseq_proteins from '<directory>/refseq_proteins.txt.seq'
-\copy refseq_identifiers from '<directory>/refseq_identifiers.txt'
-\copy refseq_nucleotides from '<directory>/refseq_nucleotides.txt'
+\copy tmp_refseq_identifiers from '<directory>/refseq_identifiers.txt'
+\copy tmp_refseq_nucleotides from '<directory>/refseq_nucleotides.txt'
 
 insert into refseq_proteins
     select accession, version,
@@ -23,17 +35,24 @@ insert into refseq_proteins
         gi, taxid, "sequence", length, false as missing
     from tmp_refseq_proteins;
 
+insert into refseq_identifiers
+    select accession, dblabel, refvalue, position, false as missing
+    from tmp_refseq_identifiers;
+
+insert into refseq_nucleotides
+    select nucleotide, protein, false as missing
+    from tmp_refseq_nucleotides;
+
 create index refseq_proteins_accession on refseq_proteins(accession);
 create index refseq_proteins_version on refseq_proteins(version);
 create index refseq_proteins_sequence on refseq_proteins(sequence);
+
 analyze refseq_proteins;
-
 analyze refseq_identifiers;
-
 analyze refseq_nucleotides;
 
 insert into refseq_nucleotide_accessions
-    select distinct nucleotide, substring(nucleotide from '[^.]*') as shortform
+    select distinct nucleotide, substring(nucleotide from '[^.]*') as shortform, false as missing
     from refseq_nucleotides;
 
 create index refseq_nucleotide_accessions_shortform on refseq_nucleotide_accessions(shortform);
