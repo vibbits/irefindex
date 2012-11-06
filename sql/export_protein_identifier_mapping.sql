f80ed73be4988004f90e763cd3ae6da49c036e8f
Paul Boddie <paul.boddie@biotek.uio.no>
2012-11-06 16:06:24 +0100

    Made the mapping cover all sequences rather than "active" ones. Fixed the coverage of interactors using different database accessions so that UniProt and RefSeq accessions are produced normally while other accession types are only produced in the absence of those principal accession types.

--- sql/export_protein_identifier_mapping.sql
+++ sql/export_protein_identifier_mapping.sql
@@ -1,55 +1,54 @@
 begin;
 
+-- Provide a mapping similar to irefindex_rogid_identifiers but covering all
+-- known sequences.
+
+create temporary table tmp_protein_identifiers as
+    select dblabel, refvalue, refsequence || reftaxid as rogid
+    from irefindex_sequences
+    where refsequence is not null and reftaxid is not null;
+
+analyze tmp_protein_identifiers;
+
+delete from tmp_protein_identifiers
+    where rogid in (
+        select rogid
+        from tmp_protein_identifiers
+        where dblabel in ('refseq', 'uniprotkb')
+        )
+        and dblabel not in ('refseq', 'uniprotkb');
+
 create temporary table tmp_protein_identifier_mapping as
     select
-        coalesce(X.dblabel, X3.dblabel) as dblabel,
-        coalesce(X.refvalue, X3.refvalue) as refvalue,
-        case when X.dblabel = 'refseq' then coalesce(cast(X2.refvalue as integer), -1) else -1 end as geneid,
+        dblabel,
+        refvalue,
+        coalesce(geneid, -1) as geneid,
         I.rog,
-        I.rogid,
+        R.rogid,
         CI.rog as crog,
         CI.rogid as crogid
 
-    -- Limit the mapping to active interactors.
-    -- The irefindex_rogids table includes source interactor details, but the
-    -- canonical table has one record per interactor.
+    -- Provide a mapping for all known sequences.
 
-    from irefindex_rogids_canonical as R
+    from tmp_protein_identifiers as R
 
     -- Obtain integer identifiers.
 
-    inner join irefindex_rog2rogid as I
+    left outer join irefindex_rog2rogid as I
         on R.rogid = I.rogid
 
     -- Obtain canonical information.
 
-    inner join irefindex_rogids_canonical as C
+    left outer join irefindex_rogids_canonical as C
         on R.rogid = C.rogid
-    inner join irefindex_rog2rogid as CI
+    left outer join irefindex_rog2rogid as CI
         on C.rogid = CI.rogid
-
-    -- RefSeq and UniProt information, if available.
-
-    left outer join irefindex_rogid_identifiers as X
-        on R.rogid = X.rogid
-        and X.dblabel in ('refseq', 'uniprotkb')
 
     -- Gene information for RefSeq records.
 
-    left outer join irefindex_rogid_identifiers as X2
-        on R.rogid = X2.rogid
-        and X2.dblabel = 'entrezgene/locuslink'
-
-    -- Other identifiers.
-
-    left outer join irefindex_rogid_identifiers as X3
-        on R.rogid = X3.rogid
-        and X3.dblabel not in ('refseq', 'uniprotkb', 'entrezgene/locuslink')
-
-    -- Choose either RefSeq/UniProt identifiers or other identifiers, but not both.
-
-    where X.dblabel is not null and X3.dblabel is null
-        or X.dblabel is null and X3.dblabel is not null;
+    left outer join irefindex_gene2refseq as G
+        on dblabel = 'refseq'
+        and refvalue = G.accession;
 
 \copy tmp_protein_identifier_mapping to '<directory>/mappings.txt'
 
