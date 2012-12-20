d167e8ad3c386a9850c1346a48d47132c47f5120
Paul Boddie <paul.boddie@biotek.uio.no>
2012-12-20 17:47:53 +0100

    Added archived sequences to the collection of known sequences from this release provided that they do not conflict with active sequences.

--- sql/export_irefindex_archived.sql
+++ sql/export_irefindex_archived.sql
@@ -4,6 +4,20 @@ create temporary table tmp_sequences_archived as
     select distinct dblabel, refvalue, reftaxid, refsequence
     from irefindex_sequences;
 
+analyze tmp_sequences_archived;
+
+-- Add archived sequences from the previous release that do not conflict with
+-- those from active data. See also import_irefindex_assignments.sql for similar
+-- work when constructing the irefindex_rogid_identifiers table.
+
+insert into tmp_sequences_archived
+    select distinct dblabel, refvalue, reftaxid, refsequence
+    from irefindex_sequences_archived as A
+    left outer join tmp_sequences_archived as S
+        on A.reftaxid || A.refsequence = S.reftaxid, S.refsequence
+        or (A.dblabel, A.refvalue) = (S.dblabel, S.refvalue)
+    where S.refvalue is null;
+
 \copy tmp_sequences_archived to '<directory>/sequences_archived'
 
 rollback;
