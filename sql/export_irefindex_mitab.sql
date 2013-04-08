82132704c87831f9030c6d98942b584edb5647bd
Paul Boddie <paul.boddie@biotek.uio.no>
2013-04-08 17:14:14 +0200

    Added copyright and licensing information.

--- sql/export_irefindex_mitab.sql
+++ sql/export_irefindex_mitab.sql
@@ -1,3 +1,20 @@
+-- Produce MITAB data from the iRefIndex build database.
+
+-- Copyright (C) 2012, 2013 Ian Donaldson <ian.donaldson@biotek.uio.no>
+-- Original author: Paul Boddie <paul.boddie@biotek.uio.no>
+--
+-- This program is free software; you can redistribute it and/or modify it under
+-- the terms of the GNU General Public License as published by the Free Software
+-- Foundation; either version 3 of the License, or (at your option) any later
+-- version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY
+-- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+-- PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 begin;
 
 -- Create a table of interactor pairs for each source database interaction.
