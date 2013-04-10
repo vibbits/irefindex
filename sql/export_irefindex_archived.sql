04cbbbf7fca9c6e8dd0f0d6a936361ba686c316d
Paul Boddie <paul.boddie@biotek.uio.no>
2013-04-10 18:39:47 +0200

    Corrected and simplified the archived sequence digests export.

--- sql/export_irefindex_archived.sql
+++ sql/export_irefindex_archived.sql
@@ -1,22 +1,36 @@
+-- Import sequences from a previous release of iRefIndex.
+
+-- Copyright (C) 2012 Ian Donaldson <ian.donaldson@biotek.uio.no>
+-- Copyright (C) 2013 Paul Boddie <paul@boddie.org.uk>
+-- Original author: Paul Boddie <paul.boddie@biotek.uio.no>
+--
+-- This program is free software; you can redistribute it and/or modify it under
+-- the terms of the GNU General Public License as published by the Free Software
+-- Foundation; either version 3 of the License, or (at your option) any later
+-- version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY
+-- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+-- PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 begin;
 
-create temporary table tmp_sequences_archived as
-    select distinct dblabel, refvalue, reftaxid, refsequence
-    from irefindex_sequences;
-
-analyze tmp_sequences_archived;
-
 -- Add archived sequences from the previous release that do not conflict with
--- those from active data. See also import_irefindex_assignments.sql for similar
--- work when constructing the irefindex_rogid_identifiers table.
+-- those from active data.
 
-insert into tmp_sequences_archived
-    select distinct dblabel, refvalue, reftaxid, refsequence
+create temporary table tmp_sequences_archived as
+    select A.dblabel, A.refvalue, A.reftaxid, A.refsequence
     from irefindex_sequences_archived as A
-    left outer join tmp_sequences_archived as S
-        on A.reftaxid || A.refsequence = S.reftaxid, S.refsequence
-        or (A.dblabel, A.refvalue) = (S.dblabel, S.refvalue)
-    where S.refvalue is null;
+    left outer join irefindex_sequences_archived as S
+        on (A.dblabel, A.refvalue) = (S.dblabel, S.refvalue)
+    where S.refvalue is null
+    union all
+    select dblabel, refvalue, reftaxid, refsequence
+    from irefindex_sequences
+    where reftaxid is not null;
 
 \copy tmp_sequences_archived to '<directory>/sequences_archived'
 
