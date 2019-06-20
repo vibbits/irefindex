862b0718ef57c1996cef130893fd58a4bce8fd51
Alexander Botzki <alexander.botzki@vib.be>
2019-06-20 09:42:08 +0200

    update version 16

--- sql/import_irefindex_sequences.sql
+++ sql/import_irefindex_sequences.sql
@@ -71,6 +71,28 @@ insert into irefindex_sequences
         on A.accession = P2.accession
     where P2.accession is null;
 
+-- sam:  copied and adapted  above code block for athaliana
+insert into irefindex_sequences
+    select distinct 'athalianabase' as dblabel, xref as refvalue,
+        taxid as reftaxid, sequence as refsequence, sequencedate as refdate
+    from athaliana_accessions as A
+    inner join uniprot_proteins as P
+        on A.xref = P.accession
+    union all
+    select distinct 'athalianabase' as dblabel, xref as refvalue,
+        P.taxid as reftaxid, P.sequence as refsequence, P.sequencedate as refdate
+    from athaliana_accessions as A
+    inner join uniprot_proteins as P
+        on A.gene_stable_id = P.uniprotid
+
+    -- Exclude previous matches.
+
+    left outer join uniprot_proteins as P2
+        on A.xref = P2.accession
+    where P2.accession is null;
+
+
+
 -- SGD accessions mapping directly and indirectly to proteins.
 
 insert into irefindex_sequences
@@ -113,26 +135,27 @@ insert into irefindex_sequences
         on A.accession = P2.accession
     where P2.accession is null;
 
+-- NOTE: gi not used anymore
 -- GenBank identifiers mapping directly and indirectly to proteins for both
 -- RefSeq and GenPept.
 
-insert into irefindex_sequences
+--insert into irefindex_sequences
 
-    -- GenBank identifiers are distinct in RefSeq and GenPept.
+  -- GenBank identifiers are distinct in RefSeq and GenPept.
 
-    select 'genbank_protein_gi' as dblabel, cast(gi as varchar) as refvalue,
-        taxid as reftaxid, sequence as refsequence, null as refdate
-    from refseq_proteins as P
-    union all
-    select 'genbank_protein_gi' as dblabel, cast(gi as varchar) as refvalue,
-        taxid as reftaxid, sequence as refsequence, null as refdate
-    from genpept_proteins as P
-    union all
-    select 'genbank_protein_gi' as dblabel, cast(gi as varchar) as refvalue,
-        P.taxid as reftaxid, P.sequence as refsequence, null as refdate
-    from genpept_accessions as A
-    inner join genpept_proteins as P
-        on A.accession = P.accession;
+--    select 'genbank_protein_gi' as dblabel, cast(gi as varchar) as refvalue,
+--        taxid as reftaxid, sequence as refsequence, null as refdate
+--    from refseq_proteins as P
+--   union all
+--    select 'genbank_protein_gi' as dblabel, cast(gi as varchar) as refvalue,
+--        taxid as reftaxid, sequence as refsequence, null as refdate
+--    from genpept_proteins as P
+--    union all
+--    select 'genbank_protein_gi' as dblabel, cast(gi as varchar) as refvalue,
+--        P.taxid as reftaxid, P.sequence as refsequence, null as refdate
+--    from genpept_accessions as A
+--    inner join genpept_proteins as P
+--        on A.accession = P.accession;
 
 -- GenBank accessions mapping directly and indirectly to proteins.
 
@@ -140,7 +163,7 @@ insert into irefindex_sequences
 
     -- Accessions and the short forms are distinct in GenPept.
 
-    select 'ddbj/embl/genbank' as dblabel, accession as refvalue,
+    select 'ddbj/embl/genbank' as dblabel, version as refvalue,
         taxid as reftaxid, sequence as refsequence, null as refdate
     from genpept_proteins as P
     union all
@@ -148,7 +171,7 @@ insert into irefindex_sequences
         taxid as reftaxid, sequence as refsequence, null as refdate
     from genpept_accessions as A
     inner join genpept_proteins as P
-        on A.accession = P.accession;
+        on A.version = P.version;
 
 -- IPI accessions mapping directly and indirectly to proteins.
 
@@ -184,7 +207,7 @@ insert into irefindex_sequences
     inner join pdb_proteins as P
         on M.accession = P.accession
         and M.chain = substring(P.chain from 1 for 1)
-	and M.gi = P.gi
+--	and M.gi = P.gi
     union all
     select distinct 'pdb' as dblabel, P.accession || '|' || substring(P.chain from 1 for 1) as refvalue,
         M.taxid as reftaxid, P.sequence as refsequence, null as refdate
@@ -200,6 +223,7 @@ insert into irefindex_sequences
 create index irefindex_sequences_index on irefindex_sequences(dblabel, refvalue);
 analyze irefindex_sequences;
 
+
 -- ROG identifiers for all sequences having a taxonomy identifier.
 
 insert into irefindex_sequence_rogids
@@ -208,22 +232,26 @@ insert into irefindex_sequence_rogids
     where reftaxid is not null;
 
 analyze irefindex_sequence_rogids;
-
+--till here no errors
 -- Actual sequences for all known sequences.
 
 insert into irefindex_sequences_original
     select distinct "sequence", actualsequence, 'uniprotkb' as dblabel
-    from uniprot_sequences
-    union all
+    from uniprot_sequences;
+-- this already gives the disk space error
+insert into irefindex_sequences_original
     select distinct "sequence", actualsequence, 'refseq' as dblabel
-    from refseq_sequences
-    union all
+    from refseq_sequences;
+
+insert into irefindex_sequences_original
     select distinct "sequence", actualsequence, 'ipi' as dblabel
-    from ipi_sequences
-    union all
+    from ipi_sequences;
+
+insert into irefindex_sequences_original
     select distinct "sequence", actualsequence, 'genpept' as dblabel
-    from genpept_sequences
-    union all
+    from genpept_sequences;
+
+insert into irefindex_sequences_original
     select distinct "sequence", actualsequence, 'pdb' as dblabel
     from pdb_sequences;
 
