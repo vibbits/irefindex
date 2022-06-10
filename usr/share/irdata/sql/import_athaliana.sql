57a44f3c0a4c6e68a9b37073923c9ef6d223daf2
lducazu <6098321+lducazu@users.noreply.github.com>
2022-06-10 11:04:31 +0200

    Database (#14)
    * Take proper care of DIP & ATHALIANA.
    * Preparations for release 53 of A thaliana.
    * SPIKE is removed for now.

--- usr/share/irdata/sql/import_athaliana.sql
+++ usr/share/irdata/sql/import_athaliana.sql
@@ -17,7 +17,51 @@
 
 begin;
 
-\copy athaliana_accessions from '<directory>/Arabidopsis_thaliana.TAIR10.46.uniprot.filtered.tsv'
+create temporary table tmp_athaliana_accessions (
+    gene_stable_id varchar NOT NULL,
+    transcript_stable_id varchar,
+    protein_stable_id varchar, -- same as transcript_stable_ID
+    xref varchar not null, -- Uniprot ID
+    db_name varchar not null,
+    info_type varchar,
+    source_identity varchar,
+    xref_identity varchar,
+    linkage_type varchar
+);
+
+\copy tmp_athaliana_accessions from '<directory>/Arabidopsis_thaliana.TAIR10.<release>.uniprot.tsv'
+
+-- Insert the _best_ row of a group of `gene_stable_id`:
+-- info_type "DEPENDENT" is better than "SEQUENCE_MATCH" and "NONE"
+
+insert into athaliana_accessions
+  with ranked_athaliana_accessions as (
+      select *,
+        rank() over (partition by gene_stable_id order by
+		          num_info_type, -- sort on the ordinal info_type
+		          protein_stable_id desc,
+		          transcript_stable_id desc,
+		          xref)
+      from (select *,
+              case info_type  -- translate info_type into an ordinal
+                when 'DEPENDENT' then 1
+                when 'SEQUENCE_MATCH' then 2
+                when 'NONE' then 3
+                else 4
+              end as num_info_type
+            from tmp_athaliana_accessions) as q)
+  select
+      gene_stable_id,
+      transcript_stable_id,
+      protein_stable_id,
+      xref,
+      db_name,
+      info_type,
+      source_identity,
+      xref_identity,
+      linkage_type
+  from ranked_athaliana_accessions
+  where rank = 1; -- keep only the top row
 
 analyze athaliana_accessions;
 
