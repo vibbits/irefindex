0e957ef6b99a3ea8cd96337f8022028d168aa09f
Alexander <38151284+abotzki@users.noreply.github.com>
2022-10-31 17:10:52 +0100

    update escaping parentheses

--- usr/bin/corrections-pre-psicquic-parsing-2022.sh
+++ usr/bin/corrections-pre-psicquic-parsing-2022.sh
@@ -3,14 +3,14 @@ perl -pi.bak -e 's/(taxid:[^\t"]+?\()([^\t"]+?[^\)])(\))(?=\t)/$1"$2"$3/g;' All.
 perl -pi.bak -e 's/(taxid:[^\t]+?\()([^\t]+?[^\)]\){1})(\){1})(?=\t)/$1"$2"$3/g;' All.mitab.08-22-2022.txt
 perl -pi.bak -e 's/(taxid:[^\t]+?\()([^\t]+?[^\)]\){1}\){1})(\){1})(?=\t)/$1"$2"$3/g;' All.mitab.08-22-2022.txt
 # correct method description
-perl -pi.bak -e 's/psi-mi:MI:1112(-)/psi-mi:"MI-1112"(two hybrid prey pooling approach)/g' All.mitab.08-22-2022.txt
-perl -pi.bak -e 's/psi-mi:MI:0397(-)/psi-mi:"MI-0397"(two hybrid assay)/g' All.mitab.08-22-2022.txt
-perl -pi.bak -e 's/psi-mi:MI:1356(-)/psi-mi:"MI-1356"(validated two hybrid)/g' All.mitab.08-22-2022.txt
-perl -pi.bak -e 's/psi-mi:"MI:0809,"(-)/psi-mi:"MI-0809"(bimolecular fluorescence complementation)/g' All.mitab.08-22-2022.txt
-perl -pi.bak -e 's/psi-mi:"MI:0000"(psi-mi:MI:0915)/psi-mi:"MI-0915"(physical association)/g' All.mitab.08-22-2022.txt
+perl -pi.bak -e 's/psi-mi:MI:1112\(-\)/psi-mi:"MI-1112"\(two hybrid prey pooling approach\)/g' All.mitab.08-22-2022.txt
+perl -pi.bak -e 's/psi-mi:MI:0397\(-\)/psi-mi:"MI-0397"\(two hybrid assay\)/g' All.mitab.08-22-2022.txt
+perl -pi.bak -e 's/psi-mi:MI:1356\(-\)/psi-mi:"MI-1356"\(validated two hybrid\)/g' All.mitab.08-22-2022.txt
+perl -pi.bak -e 's/psi-mi:"MI:0809,"\(-\)/psi-mi:"MI-0809"\(bimolecular fluorescence complementation\)/g' All.mitab.08-22-2022.txt
+perl -pi.bak -e 's/psi-mi:"MI:0000"\(psi-mi:MI:0915\)/psi-mi:"MI-0915"\(physical association\)/g' All.mitab.08-22-2022.txt
 #sed -i 's/taxid:83334(Escherichia coli O157:H7)/taxid:83334("Escherichia coli O157:H7")/g' ~/All.mitab.psicquic.06-11-2021.txt
 # correct source DB
-perl -pi.bak -e 's/psi-mi:"MI:0000"(bhf_ucl)/psi-mi:"MI:1332"(bhf-ucl)/g' All.mitab.08-22-2022.txt
+perl -pi.bak -e 's/psi-mi:"MI:0000"\(bhf_ucl\)/psi-mi:"MI:1332"\(bhf-ucl\)/g' All.mitab.08-22-2022.txt
 # correct hgnc special characters
 perl -pi.bak -e 's/(hgnc:)([^\t]+?)(\|)(\w+?:.*)(?=\t)/$1"$2"$3$4/g;' All.mitab.08-22-2022.txt
 #perl -pi.bak -e 's/(taxid:[^\t]+?\()([^\t]+?:[^\t]+?)(\))(?=\t)/$1"$2"$3/g;' ~/All.mitab.psicquic.06-11-2021.txt
