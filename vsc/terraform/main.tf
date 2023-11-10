2d1a55f7bbf086390496c50fb908173c11c3dd2c
Arthur <dev@arthurdw.com>
2023-11-10 10:19:22 +0100

    Migrate terraform irefindex to VSC (#24)
    * feat(terraform): creating the basic setup with variables and ellows ssh
    
    * Add auto generate ssh key
    
    * feat(terraform,ansible): now runs ansible code from terraform
    
    * docs(main.tf): small docs fix
    
    * Add auto generate ssh key
    
    * Add SSH
    
    * Add public_key_path
    
    * perf(main.tf): Ansible itself doesn't need to be installed on the server as it is being ran locally from a playbook
    
    * feat(main.tf|variables.tf|tfvars): added volume creation and attachement
    
    Implemented the creation of a volume and attaching this to the instance, further booting from this volume.
    
    * refactor(.gitignore) Moved the gitignore.
    
    ---------
    
    Co-authored-by: glenn.callens <glenn.callens@student.howest.be>

---
+++ vsc/terraform/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
+    }
+  }
+}
+
+provider "openstack" {
+  cloud = "openstack"
+}
+
+
+resource "openstack_compute_keypair_v2" "irefindex" {
+  name       = "${var.base_name}-key"
+  public_key = file(var.public_key_path)
+}
+
+resource "openstack_networking_port_v2" "irefindex" {
+  name           = "${var.base_name}-port"
+  network_id     = var.network_id
+  admin_state_up = true
+
+  security_group_ids = [
+    openstack_compute_secgroup_v2.ssh.id,
+  ]
+}
+
+resource "openstack_compute_secgroup_v2" "ssh" {
+  name        = "${var.base_name}-ssh"
+  description = "Allow SSH"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_blockstorage_volume_v3" "irdata" {
+  name     = "${var.base_name}-irdata-volume"
+  size     = var.storage
+  image_id = var.image_id
+}
+
+resource "openstack_compute_instance_v2" "irefindex" {
+  name        = var.base_name
+  flavor_name = var.flavor_name
+  key_pair    = openstack_compute_keypair_v2.irefindex.name
+
+  # Declaring security_groups here does nothing as it gets overwritten by the network.port!
+  # To add extra security_groups, use network.port.security_group_ids instead
+
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.irdata.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  network {
+    name = var.network_name
+    port = openstack_networking_port_v2.irefindex.id
+  }
+}
+
+resource "openstack_networking_portforwarding_v2" "ssh_pf" {
+  internal_ip_address = openstack_compute_instance_v2.irefindex.access_ip_v4
+  internal_port_id    = openstack_networking_port_v2.irefindex.id
+  floatingip_id       = var.floating_ip_id
+  external_port       = var.ssh_port
+  internal_port       = 22
+  protocol            = "tcp"
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y python3 python3-pip",
+    ]
+
+    connection {
+      host        = var.floating_ip
+      port        = var.ssh_port
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file(var.private_key_path)
+    }
+  }
+
+  provisioner "local-exec" {
+    // StrictHostKeyChecking is set to no as when running this multiple times (e.g. after a terraform destroy), the host key will change and this would fail.
+    command = "ansible-playbook -i '${var.floating_ip},' --ssh-extra-args '-p ${var.ssh_port} -o StrictHostKeyChecking=no' -u ${var.ssh_user} --private-key '${var.private_key_path}' ../ansible/hello-world.yml"
+  }
+}
+
---
+++ vsc/terraform/variables.tf
@@ -0,0 +1,67 @@
+variable "base_name" {
+  type        = string
+  description = "Base name for the instance"
+}
+
+variable "network_id" {
+  type        = string
+  description = "Network ID for the instance"
+}
+
+variable "image_id" {
+  type        = string
+  default     = "8ba6f015-e1c7-466d-bfbb-778d17811584" # Debian 11
+  description = "Image id for the instance"
+}
+
+variable "storage" {
+  type        = number
+  description = "Storage for the instance (in GB)"
+}
+
+variable "flavor_name" {
+  type        = string
+  default     = "CPUv1.small"
+  description = "Flavor name for the instance"
+
+  validation {
+    condition     = can(regex("^(CPU|GPU|UPS)v[0-9]+\\.(nano|tiny|small|medium|large|2xlarge|3xlarge|1_2xlarge|1_3xlarge|4xlarge)$", var.flavor_name))
+    error_message = "Flavor name must be one of CPUv1.nano, CPUv1.tiny, GPUv2.small, CPUv1.small, GPUv3.small, UPSv1.small, UPSv1.medium, GPUv2.medium, GPUv3.medium, CPUv1.medium, UPSv1.large, GPUv2.large, GPUv3.large, CPUv1.large, CPUv1.xlarge, UPSv1.2xlarge, GPUv2.2xlarge, GPUv3.2xlarge, CPUv1.2xlarge, CPUv1.1_2xlarge, UPSv1.3xlarge, CPUv1.3xlarge, CPUv1.1_3xlarge, CPUv1.4xlarge"
+  }
+}
+
+variable "network_name" {
+  type        = string
+  default     = "VSC_2021_102_vm"
+  description = "Network name for the instance"
+}
+
+variable "floating_ip" {
+  type        = string
+  description = "Floating IP for the instance"
+}
+
+variable "floating_ip_id" {
+  type        = string
+  description = "Floating IP ID for the instance"
+}
+
+variable "ssh_port" {
+  type        = number
+  description = "SSH port for the instance (ideally in 50k range)"
+}
+
+variable "private_key_path" {
+  type        = string
+  description = "Private key path for the instance"
+}
+
+variable "ssh_user" {
+  type        = string
+  description = "SSH user for the instance"
+}
+
+variable "public_key_path" {
+  type        = string
+  description = "Public key path "
+}
