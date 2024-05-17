e97c396bfc2a13127f87447bc318027a447e59c3
Elias Steyaert <elias.steyaert@student.howest.be>
2024-05-17 14:48:50 +0200

    recreating a part  of the playbook to do problem-solving easier/quicker

---
+++ usr/bin/pythonscript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from irdata import data as irdt
+import subprocess
+
+print(subprocess.getoutput("irdata --version"))
+
+print("would be cool if this worked")
+
+
+
+#- pythonscript: import irdata, print irdata version, print a stupid text
---
+++ usr/bin/shellscript.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+USE_PYTHON_INTERPRETER=${python_interpreter:-/usr/bin/python3}
+
+${USE_PYTHON_INTERPRETER} /home/irefindex/usr/bin/pythonscript.py
+
+
+#- shellscript: set up use_python_interpreter, run python script
